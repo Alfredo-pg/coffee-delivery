0c2853b9abcaab53d254297b45797b7e86ebecb2
Alfredo <alfredo.pamplonag@gmail.com>
2024-01-14 17:25:30 -0300

    firts commit

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    pad: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 0.875rem;
+  }
+
+  h1, h2 , h3, h4 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 800;
+  }
+`
